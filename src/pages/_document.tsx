e267ac5371f063b61e116b30b3b9c974902a2dcd
Leonardo Estevao <leooestevao@gmail.com>
2021-02-23 20:03:10 -0300

    Basic project structure

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    {/* Here is where our application will be */}
+                    <Main />
+                    {/* Here is where next will inject some auto scripts */}
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
