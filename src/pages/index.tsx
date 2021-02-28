3078f91418937c07eecb611634048f08d6a16314
Leonardo Estevao <leooestevao@gmail.com>
2021-02-28 09:59:08 -0300

    Added cookies to storage progress info (with js-cookie package)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,30 @@ import { Profile } from "../components/Profile";
 import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from 'next/head';
-
+import { GetServerSideProps } from 'next';
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+
     <div className={styles.container}>
       <Head>
         <title> Inicio | Move.It</title>
       </Head>
-      
+      {console.log(props)}
       <ExperienceBar />
 
       {/* // PS: CountdownProvider have access of ChallengesProvider, but ChallengesProvider don't have access of CountdownProvider.  
@@ -35,5 +48,23 @@ export default function Home() {
       </CountdownProvider>
       
     </div>
+</ChallengesProvider>
   )
+
+}
+
+// Everything inside this async function, will only be rendered by Server, and not the browser
+// This is the next 'intermediary' way to render, that can communicate with our server, apply logics and send the 'html' on our browser, instead of let javascript render our html and the SEO of our page (get by crawler) be 'blanc'
+// ctx = context
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
